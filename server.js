66f63161353b62ad4b460f5855a71df64ac630c6
Imesh Gunaratne <imesh.gunaratne@gmail.com>
2020-07-08 19:01:52 +1000

    Handle HTTP 403 error

--- server.js
+++ server.js
@@ -38,7 +38,11 @@ async function invokeHelloAPI() {
     } catch (err) {
         const message = 'hello-api could not respond to request: ';
         console.log(message, err.response.body);
-        throw Error(message, err.response.body);
+        if(error.response.statusCode === 403) {
+            throw Error('403 Forbidden');
+        } else {
+            throw Error(message, err.response.body);
+        }
     };
 }
 
